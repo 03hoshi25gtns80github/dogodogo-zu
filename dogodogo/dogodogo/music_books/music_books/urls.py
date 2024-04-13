2c6cbf1440c0125b354fba09c0b6c06b1d5ad1b5
hayato-matsumoto <onigiri51@iCloud.com>
2024-04-13 22:57:49 +0900

    コミットメッセージ

--- dogodogo/dogodogo/music_books/music_books/urls.py
+++ dogodogo/dogodogo/music_books/music_books/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from music_shelf.views import frontpage
 
 #受け渡し
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",frontpage)
 ]
