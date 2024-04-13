013b1bd3fdffa39bb192e13d7323f867baaf4a7a
pigu <youyah1004@gmail.com>
2024-04-14 00:54:09 +0900

    todo機能追加

--- dogodogo/dogodogo/music_books/music_shelf/models.py
+++ dogodogo/dogodogo/music_books/music_shelf/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    url = models.CharField("URL", max_length=50)
+    title = models.CharField("タイトル", max_length = 50)
+    artist = models.CharField("アーティスト名", max_length = 50)
+
+    def __str__(self):
+        return self.title
