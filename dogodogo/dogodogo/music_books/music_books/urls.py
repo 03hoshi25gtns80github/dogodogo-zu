e1d556f3e123af0568e454608a8ff75631433021
03hoshi25gtns80git <03hoshi25gtns80@gmail.com>
2024-04-13 23:19:08 +0900

    とりあえずボタンだけ表示

--- dogodogo/dogodogo/music_books/music_books/urls.py
+++ dogodogo/dogodogo/music_books/music_books/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from music_shelf.views import frontpage
+from music_shelf.views import frontpage,signup
 
 #受け渡し
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", frontpage)
+    path("", frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
 ]
--- dogodogo/dogodogo/music_books/music_shelf/views.py
+++ dogodogo/dogodogo/music_books/music_shelf/views.py
@@ -1,5 +1,22 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import SignUpForm
+from django.contrib.auth import login, authenticate
 
 def frontpage(request):
     return render(request, "music/frontpage.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+    return render(request, 'music/signup.html', {'form': form})
