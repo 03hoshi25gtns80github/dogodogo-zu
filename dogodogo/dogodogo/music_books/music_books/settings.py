64fd52ee576ec6dfcd2bc69413b97ea6968590c6
hayato-matsumoto <onigiri51@iCloud.com>
2024-04-14 09:15:09 +0900

    css

--- dogodogo/dogodogo/music_books/music_books/settings.py
+++ dogodogo/dogodogo/music_books/music_books/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +82,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Password validation
